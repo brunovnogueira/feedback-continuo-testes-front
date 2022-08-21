6a2394d60e215c536c89f8e32b27d930bc286c4a
Bruno Nogueira <brunovnogueira@gmail.com>
2022-08-21 13:03:05 -0300

    testes de criação de feedback ok

--- src/test/java/pages/PaginaFeedback.java
+++ src/test/java/pages/PaginaFeedback.java
@@ -5,11 +5,16 @@ import util.BaseTest;
 
 public class PaginaFeedback {
     private static final By campoNome = By.cssSelector("#userFeedbackSend");
-    private static final By nomeLista = By.cssSelector("#root > section > div > form > div:nth-child(1) > ul > p:nth-child(1)");
+    private static final By nomeLista = By.cssSelector("#root > section > div > form > div:nth-child(1) > ul > li");
     private static final By campoMsg = By.cssSelector("#message");
     private static final By campoTag = By.cssSelector("#tags");
+    private static final By tagLista = By.xpath("/html/body/div[1]/section/div/form/div[3]/ul[1]/li[1]");
     private static final By checkboxAnonimo = By.cssSelector("#anonymous");
     private static final By btnCriarFeedback = By.cssSelector("#root > section > div > form > button");
+    private static final By msgAtencaoNome = By.cssSelector("#id-message-collaborator");
+    private static final By msgAtencaoFeed = By.cssSelector("#id-message-feedback");
+    private static final By toastUsuarioInexistente = By.xpath("/html/body/div[1]/div/div/div/div[2]");
+
     public void preencherNome(String nome){
         BaseTest.sendKeys(campoNome,nome);
     }
@@ -32,5 +37,26 @@ public class PaginaFeedback {
 
     public void clicarNomeLista(){
         BaseTest.click(nomeLista);
+    }
+
+    public void clicarTagLista(){
+        BaseTest.click(tagLista);
+    }
+
+    public String validarMsgAtencaoNome(){
+        return BaseTest.getText(msgAtencaoNome);
+    }
+
+    public String validarMsgAtencaoFeed(){
+        return BaseTest.getText(msgAtencaoFeed);
+    }
+
+    public void clicarCampoMsg(){
+        BaseTest.click(campoMsg);
+    }
+
+    public String validarToastUsuarioInexistente(){
+        BaseTest.esperar(toastUsuarioInexistente,"Usuário não encontrado");
+        return BaseTest.getText(toastUsuarioInexistente);
     }
 }
--- src/test/java/steps/EnviarFeedbackSteps.java
+++ src/test/java/steps/EnviarFeedbackSteps.java
@@ -25,8 +25,13 @@ public class EnviarFeedbackSteps extends Browser {
     public void criarFeedbackComSucesso(){
         //Criar usuário que vai receber feedback
         paginaInicial.clicarBtnCriar();
-        String nomeReceberVar = nomeReceber;
+        String nomeReceberVar = "";
         String emailReceberVar = emailReceber;
+        if (nomeReceber.length() >= 20){
+            nomeReceberVar = "Bruno Nogueira";
+        }else {
+            nomeReceberVar = nomeReceber;
+        }
         paginaCriarConta.preencherNome(nomeReceberVar);
         paginaCriarConta.preencherEmail(emailReceberVar+"@dbccompany.com.br");
         paginaCriarConta.preencherSenha("1234@aA");
@@ -52,8 +57,8 @@ public class EnviarFeedbackSteps extends Browser {
         paginaFeedback.clicarNomeLista();
         paginaFeedback.preencherMsg("Ótimo");
         paginaFeedback.preencherTag("Java");
+        paginaFeedback.clicarTagLista();
         paginaFeedback.clicarBtnCriarFeedback();
-        paginaPrincipalLogado.clicarEnviarFeedback();
         paginaPrincipalLogado.clicarUserIcon();
         paginaPrincipalLogado.clicarSair();
 
@@ -64,6 +69,58 @@ public class EnviarFeedbackSteps extends Browser {
 
         //Validações
         Assert.assertEquals(paginaPrincipalLogado.validarNomeFeed(),nomeEnviarVar);
+        Assert.assertEquals(paginaPrincipalLogado.validarMsgFeed(),"Ótimo");
+    }
+
+    @Test
+    public void criarFeedbackAnonimoComSucesso(){
+        //Criar usuário que vai receber feedback
+        paginaInicial.clicarBtnCriar();
+        String nomeReceberVar = "";
+        String emailReceberVar = emailReceber;
+        if (nomeReceber.length() >= 20){
+            nomeReceberVar = "Bruno Nogueira";
+        }else {
+            nomeReceberVar = nomeReceber;
+        }
+        paginaCriarConta.preencherNome(nomeReceberVar);
+        paginaCriarConta.preencherEmail(emailReceberVar+"@dbccompany.com.br");
+        paginaCriarConta.preencherSenha("1234@aA");
+        paginaCriarConta.preencherConfirmarSenha("1234@aA");
+        paginaCriarConta.selecionarCargo("ANALISTA_DE_DADOS");
+        paginaCriarConta.clicarBtnCriarConta();
+        paginaPrincipalLogado.clicarUserIcon();
+        paginaPrincipalLogado.clicarSair();
+
+        //Criar usuário que vai enviar feedback
+        paginaInicial.clicarBtnCriar();
+        String nomeEnviarVar = nomeEnviar;
+        paginaCriarConta.preencherNome(nomeEnviarVar);
+        paginaCriarConta.preencherEmail(emailEnviar+"@dbccompany.com.br");
+        paginaCriarConta.preencherSenha("1234@aA");
+        paginaCriarConta.preencherConfirmarSenha("1234@aA");
+        paginaCriarConta.selecionarCargo("ANALISTA_DE_DADOS");
+        paginaCriarConta.clicarBtnCriarConta();
+
+        //Enviar o feedback
+        paginaPrincipalLogado.clicarEnviarFeedback();
+        paginaFeedback.preencherNome(nomeReceberVar);
+        paginaFeedback.clicarNomeLista();
+        paginaFeedback.preencherMsg("Ótimo");
+        paginaFeedback.preencherTag("Java");
+        paginaFeedback.clicarTagLista();
+        paginaFeedback.clicarAnonimo();
+        paginaFeedback.clicarBtnCriarFeedback();
+        paginaPrincipalLogado.clicarUserIcon();
+        paginaPrincipalLogado.clicarSair();
+
+        //Logar na conta de quem recebeu
+        paginaInicial.preencherEmail(emailReceberVar+"@dbccompany.com.br");
+        paginaInicial.preencherSenha("1234@aA");
+        paginaInicial.clicarBtnEntrar();
+
+        //Validações
+        Assert.assertEquals(paginaPrincipalLogado.validarNomeFeed(),"Anonimo");
         Assert.assertEquals(paginaPrincipalLogado.validarMsgFeed(),"Ótimo");
     }
 
@@ -98,7 +155,6 @@ public class EnviarFeedbackSteps extends Browser {
         paginaFeedback.clicarNomeLista();
         paginaFeedback.preencherMsg("Ótimo");
         paginaFeedback.clicarBtnCriarFeedback();
-        paginaPrincipalLogado.clicarEnviarFeedback();
         paginaPrincipalLogado.clicarUserIcon();
         paginaPrincipalLogado.clicarSair();
 
@@ -110,5 +166,68 @@ public class EnviarFeedbackSteps extends Browser {
         //Validações
         Assert.assertEquals(paginaPrincipalLogado.validarNomeFeed(),nomeEnviarVar);
         Assert.assertEquals(paginaPrincipalLogado.validarMsgFeed(),"Ótimo");
+    }
+
+    @Test
+    public void criarFeedbackSemDestinatario(){
+        //Criar usuário que vai enviar feedback
+        paginaInicial.clicarBtnCriar();
+        String nomeEnviarVar = nomeEnviar;
+        paginaCriarConta.preencherNome(nomeEnviarVar);
+        paginaCriarConta.preencherEmail(emailEnviar+"@dbccompany.com.br");
+        paginaCriarConta.preencherSenha("1234@aA");
+        paginaCriarConta.preencherConfirmarSenha("1234@aA");
+        paginaCriarConta.selecionarCargo("ANALISTA_DE_DADOS");
+        paginaCriarConta.clicarBtnCriarConta();
+
+        //Enviar o feedback
+        paginaPrincipalLogado.clicarEnviarFeedback();
+        paginaFeedback.preencherMsg("Ótimo");
+
+        //Validações
+        Assert.assertEquals(paginaFeedback.validarMsgAtencaoNome(),"Obrigatório preencher.");
+    }
+
+    @Test
+    public void criarFeedbackSemMensagem(){
+        //Criar usuário que vai enviar feedback
+        paginaInicial.clicarBtnCriar();
+        String nomeEnviarVar = nomeEnviar;
+        paginaCriarConta.preencherNome(nomeEnviarVar);
+        paginaCriarConta.preencherEmail(emailEnviar+"@dbccompany.com.br");
+        paginaCriarConta.preencherSenha("1234@aA");
+        paginaCriarConta.preencherConfirmarSenha("1234@aA");
+        paginaCriarConta.selecionarCargo("ANALISTA_DE_DADOS");
+        paginaCriarConta.clicarBtnCriarConta();
+
+        //Enviar o feedback
+        paginaPrincipalLogado.clicarEnviarFeedback();
+        paginaFeedback.preencherNome("Teste");
+        paginaFeedback.clicarCampoMsg();
+
+        //Validações
+        Assert.assertEquals(paginaFeedback.validarMsgAtencaoFeed(),"Obrigatório preencher.");
+    }
+
+    @Test
+    public void criarFeedbackUsuarioInexistente(){
+        //Criar usuário que vai enviar feedback
+        paginaInicial.clicarBtnCriar();
+        String nomeEnviarVar = nomeEnviar;
+        paginaCriarConta.preencherNome(nomeEnviarVar);
+        paginaCriarConta.preencherEmail(emailEnviar+"@dbccompany.com.br");
+        paginaCriarConta.preencherSenha("1234@aA");
+        paginaCriarConta.preencherConfirmarSenha("1234@aA");
+        paginaCriarConta.selecionarCargo("ANALISTA_DE_DADOS");
+        paginaCriarConta.clicarBtnCriarConta();
+
+        //Enviar o feedback
+        paginaPrincipalLogado.clicarEnviarFeedback();
+        paginaFeedback.preencherNome("TESTE");
+        paginaFeedback.preencherMsg("Ótimo");
+        paginaFeedback.clicarBtnCriarFeedback();
+
+        //Validações
+        Assert.assertEquals(paginaFeedback.validarToastUsuarioInexistente(),"Usuário não encontrado");
     }
 }
